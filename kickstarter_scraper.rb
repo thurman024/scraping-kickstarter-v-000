76d6b467a68868ef7e962469b6510266d86638db
Stephen Thurman <thurman024@gmail.com>
2018-02-01 16:35:52 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,6 +5,7 @@ def create_project_hash
 
   kickstarter = Nokogiri::HTML(html)
   #projects: kickstarter.css("li.project.grid_4")
+  #title: project.css("h2.bbcard_name strong a").text
   binding.pry
 end
 create_project_hash
