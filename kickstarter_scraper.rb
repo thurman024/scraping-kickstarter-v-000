aac5d4020d940154236b9deb92b3d2ca730d6596
Stephen Thurman <thurman024@gmail.com>
2018-02-01 17:08:35 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -16,8 +16,9 @@ def create_project_hash
       image_link: project.css("div.project-thumbnail a img").attribute("src").value,
       description: project.css("p.bbcard_blurb").text
       location: project.css("ul.project-meta span.location-name").text
-      
+      percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
+  end
   binding.pry
 end
 create_project_hash
